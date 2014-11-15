5fba0b62ff4ee9ccf74e79861b45cfb4e5cb1507
andytt <tta02303991@gmail.com>
2014-11-15 10:10:33 +0800

    Add async.js.
    href: https://github.com/caolan/async

--- Twitter.js
+++ Twitter.js
@@ -1,53 +1,46 @@
-var url = require('url'),
-    querystring = require('querystring'),
-    http = require('restler'),
-    Lazy = require('lazy.js'),
-    Twitter = require('twitter-js-client').Twitter,
-    configs = require('./configs.js');
+(function () {
+    var url = require('url'),
+        querystring = require('querystring'),
+        Lazy = require('lazy.js'),
+        Twitter = require('twitter-js-client').Twitter,
+        configs = require('./configs.js'),
+        URL = require('./services/URL.js'),
+        async = require('async');
 
-if (process.argv.length < 3) {
-    console.log('You must provide a Twitter screen name.');
-    process.exit(1);
-}
+    if (process.argv.length < 3) {
+        console.log('You must provide a Twitter screen name.');
+        process.exit(1);
+    }
 
-var client = new Twitter({
-    consumerKey: configs.key,
-    consumerSecret: configs.secret
-});
+    var client = new Twitter({
+        consumerKey: configs.Twitter.key,
+        consumerSecret: configs.Twitter.secret
+    });
 
-var screenName = process.argv[2],
-    error = function (err) {
-        console.log('Error', err);
-    },
-    success = function (data) {
-        var urls = [];
+    var screenName = process.argv[2],
+        error = function (err) {
+            console.log(err);
+        },
+        success = function (data) {
+            var urls = [];
 
-        JSON.parse(data).forEach(function (status) {
-            if (!status.entities.media) return false;
-            urls = urls.concat(Lazy(status.entities.media).pluck('media_url').toArray());
-        });
+            JSON.parse(data).forEach(function (status) {
+                if (!status.entities.media) return false;
+                urls = urls.concat(Lazy(status.entities.media).pluck('media_url').toArray());
+            });
 
-        urls = Lazy(urls).chunk(10).toArray();
+            urls = Lazy(urls).chunk(10).toArray();
 
-        var saveUrls = function (urls) {
-            return http.post('http://lin.andytt.org:1337/url/new', {
-                data: {
-                    urls: urls
-                }
+            async.eachSeries(urls, function (url, callback) {
+                URL.save(url).on('complete', function (rep) {
+                    callback();
+                });
             });
         };
 
-        var cfn = function (rep) {
-            if (!urls.length) return true;
-
-            saveUrls(urls.pop()).on('complete', cfn);
-        };
-
-        saveUrls(urls.pop()).on('complete', cfn);
-    };
-
-client.getUserTimeline({
-    screen_name: screenName,
-    count: 200,
-    include_rts: true
-}, error, success);
+    client.getUserTimeline({
+        screen_name: screenName,
+        count: 200,
+        include_rts: true
+    }, error, success);
+})();
